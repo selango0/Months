ae7fbf38e482b6dc921ce5d69734aa9d461abfd9
selango2017 <shalmika.elango@gmail.com>
2017-03-23 18:53:53 -0400

    Update Months.cpp

--- Months.cpp
+++ Months.cpp
@@ -1,143 +1,92 @@
-#include "Month.h"
+
 #include<iostream>
 #include <string>
-
+#include "Month.h"
 
 using namespace std; 
 
 
-void Month:: setMonthName(const string name)
+void Month:: setMonthName(int num)
 {
-	if (name == "January" || name == "Jan") setMonthNumber(1);
-	else if (name == "February" || name == "Feb")
-	{
-		setMonthNumber(2);
-	}
-	else if (name == "March" || name == "Mar")
-	{
-		setMonthNumber(3);
-	}
-	else if (name == "April" || name == "Apr")
-	{
-		setMonthNumber(4);
-	}
-	else if (name == "May")
-	{
-		setMonthNumber(5);
-	}
-	else if (name == "June" || name == "Jun")
-	{
-		setMonthNumber(6);
-	}
-	else if (name == "July" || name == "Jul")
-	{
-		setMonthNumber(7);
-	}
-	else if (name == "August" || name == "Aug")
-	{
-		setMonthNumber(8);
-	}
-	else if (name == "September" || name == "Sept")
-	{
-		setMonthNumber(9);
-	}
-	else if (name == "October" || name == "Oct")
-	{
-		setMonthNumber(10);
-	}
-	else if (name == "November" || name == "Nov")
-	{
-		setMonthNumber(11);
-	}
-	else if (name == "December" || name == "Dec")
-	{
-		setMonthNumber(12);
-	}
-	else
-	{
-		setMonthNumber(1);
-	}
+	if (num == 1)
+		monthName = "January";
+	else if (num == 2)
+		monthName = "Febuary";
+	else if (num == 3)
+		monthName = "March";
+	else if (num == 4)
+		monthName = "April";
+	else if (num == 5)
+		monthName = "May";
+	else if (num = 6)
+		monthName = "June";
+	else if (num == 7)
+		monthName = "July";
+	else if (num == 8)
+		monthName == "August";
+	else if (num == 9)
+		monthName == "September";
+	else if (num == 10)
+		monthName == "October";
+	else if (num == 11)
+		monthName == "November";
+	else 
+		monthName == "December";
 
 }
 
-void Month:: setMonthNumber(const int num)
+void Month::setMonthNumber(string nm)
 {
-	if (num < 1 || num > 12)
+	if (nm == "January")
 		monthNumber = 1;
-	else
-		monthNumber = num;
+	else if (nm == "February")
+		monthNumber = 2;
 
-	if (monthNumber == 1)
-		monthName = "January";
-	else if (monthNumber == 2)
-		monthName = "February";
-	else if (monthNumber == 3)
-		monthName = "March";
-	else if (monthNumber == 4)
-		monthName = "April";
-	else if (monthNumber == 5)
-		monthName = "May";
-	else if (monthNumber == 6)
-		monthName = "June";
-	else if (monthNumber == 7)
-		monthName = "July";
-	else if (monthNumber == 8)
-		monthName = "August";
-	else if (monthNumber == 9)
-		monthName = "September";
-	else if (monthNumber == 10)
-		monthName = "October";
-	else if (monthNumber == 11)
-		monthName = "November";
-	else
-		monthName = "December";
+	else if (nm == "March")
+		monthNumber = 3;
+	else if (nm == "April")
+		monthNumber = 4;
+	else if (nm == "May")
+		monthNumber = 5;
+	else if (nm == "June")
+		monthNumber = 6;
+	else if (nm == "July")
+		monthNumber = 7;
+	else if (nm == "August")
+		monthNumber = 8;
+	else if (nm == "September")
+		monthNumber = 9;
+	else if (nm == "October")
+		monthNumber = 10;
+	else if (nm == "November")
+		monthNumber = 11;
+	else if (nm == "December")
+		monthNumber = 12;
+	
 }
 
 Month Month::operator++()
 {
-	if (monthNumber == 12)
+	monthNumber++;
+	if (monthNumber == 13)
+	{
 		monthNumber = 1;
-	else
-		++monthNumber;
-	setMonthNumber(monthNumber);
+		monthName = "January";
+	}
+	setMonthName(monthNumber);
 	return *this;
-}
-
-Month Month::operator++(int)
-{
-	Month temp(monthNumber);
-	setMonthNumber(++monthNumber);
-	return temp;
 }
 
 Month Month::operator--()
 {
-	if (monthNumber == 1)
-		monthNumber = 12;
-	else --monthNumber;
-	setMonthNumber(monthNumber);
+	monthNumber--;
+	if (monthNumber == 0)
+	{
+		monthNumber = 12; 
+		monthName = "December"; 
+
+	}
+	setMonthName(monthNumber);
 	return *this;
 }
 
-Month Month::operator--(int)
-{
-	Month temp(monthNumber);
-	setMonthNumber(--monthNumber);
-	return temp;
-}
-
-ostream &operator << (ostream &strm, const Month &obj)
-{
-	return strm << obj.monthName << "(" << obj.monthNumber << ")";
-}
-
-istream &operator >> (istream &strm, Month &monthObj)
-{
-	string m_Name;
-	cout << endl << "Please enter the month name: " << endl;
-	strm >> m_Name;
-	monthObj.setMonthName(m_Name);
-	return strm;
-}
-
-	
